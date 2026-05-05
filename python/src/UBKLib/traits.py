9c25c6dc4fc6f42280d4ef5d78745df9e364ad01
Ashley <ajkhsu22@gmail.com>
2026-05-05 18:31:28 +0100

    Added new W0 contour function type to the traits and added a ubcorrd dataclass

--- python/src/UBKLib/traits.py
+++ python/src/UBKLib/traits.py
@@ -2,6 +2,7 @@ from collections.abc import Callable
 from typing import Union
 import numpy.typing as npt
 import numpy as np
+from dataclasses import dataclass
 
 Vectorizable = Union[float, npt.NDArray[np.float64]]
 
@@ -12,3 +13,15 @@ PotentialFunction = Callable[
     ],
     Vectorizable
 ]
+
+W0ContourFunction = Callable[
+    [
+        Vectorizable
+    ],
+    Vectorizable
+]
+
+@dataclass
+class UBCoord:
+    B: float
+    U: float
