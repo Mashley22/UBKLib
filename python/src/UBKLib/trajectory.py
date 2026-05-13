c68ca03f5e21882d4eb43a0e18b14b2f0f34a8be
Ashley <ajkhsu22@gmail.com>
2026-05-13 20:24:48 +0100

    Fixed the lcds trajectory finder, now handles the positively charged regions properly

--- python/src/UBKLib/trajectory.py
+++ python/src/UBKLib/trajectory.py
@@ -61,10 +61,10 @@ def relativistic_ub_trajectory(
         the values U(B_m) for the given parameters
     """
 
-    return intercept - rest_mass * np.sqrt(1 + 2 * mu * B_m / rest_mass) / charge
+    return intercept - rest_mass * np.sqrt(1 + 2 * mu * B_m / rest_mass) / charge + rest_mass / charge
 
 
-def __continuous_lcds_ub_intercept(
+def __continuous_lcds_ub_min_intercept(
         lower_contour: W0ContourFunction,
         upper_contour: W0ContourFunction,
         trajectory: Callable[[float], float],
@@ -72,18 +72,11 @@ def __continuous_lcds_ub_intercept(
         B_bounds: tuple[float, float]
 ) -> float:
 
-    if charge < 0:
-        def lower_minize_func(x):
-            return trajectory(x) - lower_contour(x)
+    def lower_minize_func(x):
+        return -trajectory(x) + lower_contour(x)
 
-        def upper_minize_func(x):
-            return trajectory(x) - upper_contour(x)
-    else:
-        def lower_minize_func(x):
-            return -trajectory(x) + lower_contour(x)
-
-        def upper_minize_func(x):
-            return -trajectory(x) + upper_contour(x)
+    def upper_minize_func(x):
+        return -trajectory(x) + upper_contour(x)
     
     lower_result = minimize_scalar(lower_minize_func,
                                    bounds=B_bounds,
@@ -101,26 +94,59 @@ def __continuous_lcds_ub_intercept(
     if upper_result.success is False:
         raise RuntimeError(f"Scipy minimization failed: {upper_result.message}")
 
-    if charge < 0:
-        return min(-lower_result.fun, -upper_result.fun)
-    else:
-        return max(lower_result.fun, upper_result.fun)
+    return max(lower_result.fun, upper_result.fun)
 
 
-def continuous_lcds_ub_trajectory(
+def __continuous_lcds_ub_max_intercept(
+        lower_contour: W0ContourFunction,
+        upper_contour: W0ContourFunction,
+        trajectory: Callable[[float], float],
+        charge: int, 
+        B_bounds: tuple[float, float]
+) -> float:
+
+    def lower_minize_func(x):
+        return trajectory(x) - lower_contour(x)
+
+    def upper_minize_func(x):
+        return trajectory(x) - upper_contour(x)
+    
+    lower_result = minimize_scalar(lower_minize_func,
+                                   bounds=B_bounds,
+                                   method='bounded',
+                                   options=__MINIZER_OPTIONS)
+
+    if lower_result.success is False:
+        raise RuntimeError(f"Scipy minimization failed: {lower_result.message}")
+
+    upper_result = minimize_scalar(upper_minize_func,
+                                   bounds=B_bounds,
+                                   method='bounded',
+                                   options=__MINIZER_OPTIONS)
+
+    if upper_result.success is False:
+        raise RuntimeError(f"Scipy minimization failed: {upper_result.message}")
+
+    return min(-lower_result.fun, -upper_result.fun)
+
+
+def __continuous_lcds_ub_trajectory(
         lower_contour: W0ContourFunction,
         upper_contour: W0ContourFunction,
         trajectory: Callable[[float], float],
         charge: int,
+        intercept: float,
         B_bounds: tuple[float, float] = (dipole.SURFACE_STRENGTH / (15 ** 3), dipole.SURFACE_STRENGTH / (1.05 ** 3))
 ) -> UBTrajectory:
-    """Calculates the the LCDS trajectory for a particle with a given gradient.
+    """Calculates the the closed trajectory W=0 intercepts for a particle with a given trajectory, charge and intercept
     
     Args:
         lower_contour: a function that describes the lower W=0 contour as a function U(B)
         upper_contour: same as lower_contour but for the upper W=0 contour
-        gradient(kV/nT): the value of the -mu/q for the given particle
-        B_bounds(nT): the bounds for the value of B to search within, by default chooses field delta as the equatorial strength
+        trajectory: the U(B_m) for a general particle with the same properties
+        charge (e): the charge of the particle
+        intercept (kV): the constant term in the trajectory
+        B_bounds (nT): the bounds for the value of B to search within, by default chooses field delta as the equatorial strength
                       at distances 15 to 1.05 Re
     Raises:
         RuntimeError: if the scipy minizer fails
@@ -130,7 +156,7 @@ def continuous_lcds_ub_trajectory(
     """
 
     trajectoryInf = UBTrajectory()
-    trajectoryInf.intercept = __continuous_lcds_ub_intercept(lower_contour, upper_contour, trajectory, charge, B_bounds)
+    trajectoryInf.intercept = intercept
 
     lower_result = minimize_scalar(lambda x: (trajectory(x) + trajectoryInf.intercept - lower_contour(x)) ** 2,
                                    bounds=B_bounds,
@@ -152,3 +178,37 @@ def continuous_lcds_ub_trajectory(
     trajectoryInf.upper_intercept = UBCoord(B=upper_result.x, U=upper_contour(upper_result.x))
 
     return trajectoryInf
+
+
+def continuous_lcds_ub_trajectory(
+        lower_contour: W0ContourFunction,
+        upper_contour: W0ContourFunction,
+        trajectory: Callable[[float], float],
+        charge: int,
+        B_bounds: tuple[float, float] = (dipole.SURFACE_STRENGTH / (15 ** 3), dipole.SURFACE_STRENGTH / (1.05 ** 3))
+) -> UBTrajectory:
+    """Calculates the the LCDS trajectory for a particle with a given trajectory, charge
+    
+    Args:
+        lower_contour: a function that describes the lower W=0 contour as a function U(B)
+        upper_contour: same as lower_contour but for the upper W=0 contour
+        trajectory: the U(B_m) for a general particle with the same properties
+        charge (e): the charge of the particle
+        B_bounds (nT): the bounds for the value of B to search within, by default chooses field delta as the equatorial strength
+                      at distances 15 to 1.05 Re
+    Raises:
+        RuntimeError: if the scipy minizer fails
+
+    Returns:
+        The trajectory information
+    """
+    minIntercept = __continuous_lcds_ub_min_intercept(lower_contour, upper_contour, trajectory, charge, B_bounds)
+    maxIntercept = __continuous_lcds_ub_max_intercept(lower_contour, upper_contour, trajectory, charge, B_bounds)
+
+    minTrajectory = __continuous_lcds_ub_trajectory(lower_contour, upper_contour, trajectory, charge, minIntercept, B_bounds)
+    maxTrajectory = __continuous_lcds_ub_trajectory(lower_contour, upper_contour, trajectory, charge, maxIntercept, B_bounds)
+
+    if minTrajectory.lower_intercept.B < maxTrajectory.lower_intercept.B:
+        return minTrajectory
+
+    return maxTrajectory
