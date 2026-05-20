78dedbc07364032582fc6e92bc89d8ee3c5527ad
Ashley <ajkhsu22@gmail.com>
2026-05-20 19:59:48 +0100

    Updated the analytical models, including the volland stern model, and updated the cross tail model for better guarantees of the lower and upper contours.

--- python/src/UBKLib/analytical_case.py
+++ python/src/UBKLib/analytical_case.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-from .field_models import cross_tail, dipole
+from .field_models import (
+    cross_tail,
+    dipole,
+    volland_stern
+)
 from .types import Vectorizable, UBTrajectory
 from .trajectory import continuous_lcds_ub_trajectory
 
 
-class UBCurve:
+class CrossTailAndDipole:
     '''
     This case is for the cross tail potential combined with a dipole 
     magnetic field at K=0, the same case as supplied in Whipple.
@@ -45,8 +49,12 @@ class UBCurve:
         Returns:
             electric potential (kV)
         '''
-        return self.cross_tail_strength * np.pow(self.dipole_strength / magnetic_amplitude, 1 / 3) \
-            - self.surface_potential * np.pow(magnetic_amplitude / self.dipole_strength, 1 / 3)
+        return (
+            np.abs(
+                self.cross_tail_strength * np.pow(self.dipole_strength / magnetic_amplitude, 1 / 3)
+            ) -
+            self.surface_potential * np.pow(magnetic_amplitude / self.dipole_strength, 1 / 3)
+        )
 
     def lower_contour(self,
                       magnetic_amplitude: Vectorizable) -> Vectorizable:
@@ -60,8 +68,116 @@ class UBCurve:
         Returns:
             electric potential (kV)
         '''
-        return -self.cross_tail_strength * np.pow(self.dipole_strength / magnetic_amplitude, 1 / 3) \
-            - self.surface_potential * np.pow(magnetic_amplitude / self.dipole_strength, 1 / 3)
+        return (
+            -np.abs(
+                self.cross_tail_strength * np.pow(self.dipole_strength / magnetic_amplitude, 1 / 3)
+            ) -
+            self.surface_potential * np.pow(magnetic_amplitude / self.dipole_strength, 1 / 3)
+        )
+
+    def radius(self,
+               magnetic_amplitude: Vectorizable) -> Vectorizable:
+        '''
+        Calculates the value of the radius for a given value of the magnetic field amplitude
+
+        Parameters:
+            magnetic_amplitude (nT)
+
+        Returns:
+            radius (Re)
+        '''
+
+        return np.pow(self.dipole_strength / magnetic_amplitude, 1 / 3)
+
+    def lcds_line(self, gradient: float) -> UBTrajectory:
+        '''
+        Calculates the UB coordinates of the LCDS trajectory in the UB space where this trajectory
+        meets the W=0 contours
+
+        Parameters:
+            gradient (kV/nT): the value of -mu/q
+
+        Returns:
+            B1, U1, B2, U2 where B1, U1 refer to the point where this LCDS trajectory meets the lower curve.
+            Likewise B2, U2, refer to upper curve meeting point.
+        '''
+
+        return continuous_lcds_ub_trajectory(
+            self.lower_contour,
+            self.upper_contour,
+            gradient,
+            (
+                dipole.equatorial_dipole_amplitude(self.outer_lim, 0, self.dipole_strength),
+                dipole.equatorial_dipole_amplitude(self.inner_lim, 0, self.dipole_strength),
+            )
+        )
+
+
+class VollandSternAndDipole:
+    '''
+    This case is for the volland stern potential combined with a dipole 
+    magnetic field at K=0.
+    In this case the U-B curves can be found analytically and as such the 
+    trajectories in U-B space can also be found analytically, the extrema points 
+    in U-B space can be found analytically as well.
+    '''
+
+    surface_potential: float
+    convection_strength: float
+    dipole_strength: float
+    inner_lim: float
+    outer_lim: float
+
+    def __init__(self,
+                 surface_potential: float = volland_stern.SURFACE_POTENTIAL,
+                 convection_strength: float = volland_stern.CONVECTION_NUMERATOR,
+                 dipole_strength: float = dipole.SURFACE_STRENGTH,
+                 inner_lim: float = 1.05,
+                 outer_lim: float = 15.0):
+                         
+        self.surface_potential = surface_potential
+        self.convection_strength = convection_strength
+        self.dipole_strength = dipole_strength
+        self.inner_lim = inner_lim
+        self.outer_lim = outer_lim
+
+    def upper_contour(self,
+                      magnetic_amplitude: Vectorizable) -> Vectorizable:
+        '''
+        Calculates the value of the electric potential upper value for a given
+        magnetic field amplitude
+        
+        Parameters:
+            magnetic_amplitude: in nT
+
+        Returns:
+            electric potential (kV)
+        '''
+        return (
+            np.abs(
+                self.convection_strength * np.pow(self.dipole_strength / magnetic_amplitude, 2 / 3)
+            ) -
+            self.surface_potential * np.pow(magnetic_amplitude / self.dipole_strength, 1 / 3)
+        )
+
+    def lower_contour(self,
+                      magnetic_amplitude: Vectorizable) -> Vectorizable:
+        '''
+        Calculates the value of the electric potential lower value for a given
+        magnetic field amplitude
+        
+        Parameters:
+            magnetic_amplitude (nT)
+
+        Returns:
+            electric potential (kV)
+        '''
+        return (
+            -np.abs(
+                self.convection_strength * np.pow(self.dipole_strength / magnetic_amplitude, 2 / 3)
+            ) -
+            self.surface_potential * np.pow(magnetic_amplitude / self.dipole_strength, 1 / 3)
+        )
 
     def radius(self,
                magnetic_amplitude: Vectorizable) -> Vectorizable:
